bdff1dbb25ac13dc820bb8f40a980b58ce8e8818
anannya136 <169048592+anannya136@users.noreply.github.com>
2025-06-28 23:06:59 +0600

    Add files via upload

---
+++ Transport_management.java
@@ -0,0 +1,11 @@
+package transport_management;
+
+public class Transport_management {
+
+    
+    public static void main(String[] args) {
+        MainScreen mr=new MainScreen();
+        mr.setVisible(true);
+    }
+
+}
